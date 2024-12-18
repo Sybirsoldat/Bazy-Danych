ec56ea9f424b40b6876a5e169941f32d027a54ea
Piotr <272503@student.pwr.edu.pl>
2024-12-18 01:17:09 +0100

    dodanie funkcji do repo tabeli book

--- library/src/main/java/com/example/library/repositories/BookRepository.java
+++ library/src/main/java/com/example/library/repositories/BookRepository.java
@@ -4,5 +4,12 @@ import com.example.library.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface BookRepository extends JpaRepository<Book, Long> { }
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByTitleContainingIgnoreCase(String title);
+    List<Book> findByGenre_Id(Long genreId);
+    List<Book> findByPublishYear(int year);
+    List<Book> findByPriceBetween(int minPrice, int maxPrice);
+}
