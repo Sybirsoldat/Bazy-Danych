a83c1767b9d2719491907b8dcfcf61dd480d0f13
Piotr <272503@student.pwr.edu.pl>
2024-12-18 01:16:57 +0100

    dodanie funkcji do serwisu tabeli book

--- library/src/main/java/com/example/library/services/BookService.java
+++ library/src/main/java/com/example/library/services/BookService.java
@@ -12,11 +12,56 @@ public class BookService {
     @Autowired
     private BookRepository bookRepository;
 
-    public List<Book> getAllBooks() { return bookRepository.findAll(); }
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
 
-    public Book getBookById(Long id) { return bookRepository.findById(id).orElseThrow(); }
+    public Book createBook(Book book) {
+        return bookRepository.save(book);
+    }
 
-    public Book createBook(Book book) { return bookRepository.save(book); }
+    public Book getBookById(Long id) {
+        return bookRepository.findById(id).orElseThrow(() -> new RuntimeException("Book not found"));
+    }
 
-    public void deleteBook(Long id) { bookRepository.deleteById(id); }
+    public List<Book> getBooksByTitle(String title) {
+        return bookRepository.findByTitleContainingIgnoreCase(title);
+    }
+
+    public List<Book> getBooksByGenre(Long genreId) {
+        return bookRepository.findByGenre_Id(genreId);
+    }
+
+    public List<Book> getBooksByPublishYear(int year) {
+        return bookRepository.findByPublishYear(year);
+    }
+
+    public List<Book> getBooksByPriceRange(int minPrice, int maxPrice) {
+        return bookRepository.findByPriceBetween(minPrice, maxPrice);
+    }
+
+    public Book updateBookPrice(Long id, int newPrice) {
+        Book book = getBookById(id);
+        book.setPrice(newPrice);
+        return bookRepository.save(book);
+    }
+
+    public Book updateBookAvailability(Long id, int totalAmount) {
+        Book book = getBookById(id);
+        if (totalAmount < 0) {
+            throw new RuntimeException("Total amount cannot be negative");
+        }
+        book.setTotalAmount(totalAmount);
+        return bookRepository.save(book);
+    }
+
+    public Book softDeleteBook(Long id) {
+        Book book = getBookById(id);
+        book.setDeleted(true);
+        return bookRepository.save(book);
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
 }
